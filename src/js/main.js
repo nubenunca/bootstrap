eae40d3bdc8ec17a5664457f3dc4681e7e81cbb0
nubenunca <marylinrestrepp@gmail.com>
2024-05-04 23:47:51 -0500

    mejoras en el copy

--- src/js/main.js
+++ src/js/main.js
@@ -9,7 +9,7 @@ const cardContainer = document.getElementById("card-container");
 header.innerHTML = `
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container-fluid">
-        <a class="navbar-brand" href="/pages/biaipets.html">PetLove</a>
+        <a class="navbar-brand" href="/pages/biaipets.html">BIAIPETS</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
